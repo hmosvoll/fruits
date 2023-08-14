a3b84b29dca06671ca6e58078fe8be97c534f546
Håkon Mosvoll <hakon.mosvoll@payex.com>
2023-08-14 14:36:54 +0200

    Add simple functionality for adding a fruit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	type Product struct {
+		Name string
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("index.html"))
+
+		products := []Product{{Name: "Apple"}, {Name: "Banana"}, {Name: "Orange"}}
+
+		template.Execute(w, products)
+	})
+
+	http.HandleFunc("/fruits", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second)
+
+		fruit := r.PostFormValue("name")
+
+		template := template.Must(template.ParseFiles("index.html"))
+		template.ExecuteTemplate(w, "fruit", Product{Name: fruit})
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
